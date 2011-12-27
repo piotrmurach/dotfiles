014d53e42ffb7001ec0771107d8386b944b378bd
Piotr Murach <pmurach@gmail.com>
2011-12-27 15:06:33 +0000

    Add features stubs for thor tasks testing.

---
+++ features/support/env.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../*')
+require 'aruba/cucumber'
+require 'fileutils'
+require 'rspec/expectations'
---
+++ features/vim_tasks.feature
@@ -0,0 +1,5 @@
+Feature: Vim tasks
+
+  In order to specify tasks that handle vim installation
+  As a developer
+  I want to be able to issue them
