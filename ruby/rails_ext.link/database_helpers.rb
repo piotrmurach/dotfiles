c6d9615e350be034bc91db1f30ab669fcf4e66c8
Piotr Murach <pmurach@gmail.com>
2012-01-07 21:40:25 +0000

    Add rails irb database extensions.

--- ruby/rails_ext.link/database_helpers.rb
+++ ruby/rails_ext.link/database_helpers.rb
@@ -33,3 +33,45 @@ def define_model_find_shortcuts
 end
 
 IRB.conf[:IRB_RC] = Proc.new { define_model_find_shortcuts }
+
+if defined? Hirb
+  def tables
+    Hirb::Console.render_output(
+      ActiveRecord::Base.connection.tables.map { |e| [e] },
+      {
+        :class => Hirb::Helpers::AutoTable,
+        :headers => %w<tables>
+      }
+    )
+    true
+  end
+
+  # Provide database style table view
+  def table table_name
+    Hirb::Console.render_output(
+      ActiveRecord::Base.connection.columns(table_name).map do |e|
+       [ e.name, e.type, e.sql_type, e.limit, e.default,
+         e.scale, e.precision, e.primary, e.null]
+      end,
+      {
+        :class   => Hirb::Helpers::AutoTable,
+        :headers => %w<name type sql_type limit default scale precision primary null>
+      }
+    )
+    true
+  end
+
+  # Provide overview of table counts
+  def counts
+    Hirb::Console.render_output(
+      ActiveRecord::Base.connection.tables.map do |e|
+        [e, ActiveRecord::Base.connection.select_value("SELECT COUNT(*) FROM #{e}")]
+      end,
+      {
+        :class   => Hirb::Helpers::AutoTable,
+        :headers => %w<table count>,
+      }
+    )
+    true
+  end
+end
