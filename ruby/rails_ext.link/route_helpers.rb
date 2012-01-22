c85adcc6410737fd19d92bdbbbf12d671825f69a
Piotr Murach <pmurach@gmail.com>
2012-01-22 21:35:25 +0000

    Comment out buggy code.

--- ruby/rails_ext.link/route_helpers.rb
+++ ruby/rails_ext.link/route_helpers.rb
@@ -1,7 +1,7 @@
 # Extensions for routes viewing inside rails irb
 
-require 'action_dispatch/routing/routes'
+# require 'action_dispatch/routing/routes'
 # Access to routes set
-def list_routes
-  ActionController::Routing::Routes
-end
+# def list_routes
+#   ActionController::Routing::Routes
+# end
